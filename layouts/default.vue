4421338afd67a5ac9be0f46c6daec03b6122ec24
Alex Segundo <asegundo442@gmail.com>
2021-01-12 22:26:06 -0500

    correction dependencys

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,14 @@
     <Fragment>
         <AppNavigation />
         <main class="center-app">
-            <nuxt/>
+            <nuxt />
         </main>
         <AppFooter />
-        <script id="dsq-count-scr" src="//cp-janque-com.disqus.com/count.js" async></script>
+        <script
+            id="dsq-count-scr"
+            src="//cp-janque-com.disqus.com/count.js"
+            async
+        ></script>
     </Fragment>
 </template>
 
@@ -18,86 +22,91 @@ export default {
     components: {
         Fragment,
         AppNavigation,
-        AppFooter
+        AppFooter,
     },
-    head: () =>({
-        link: [ { hid:'favicon', rel:'icon', href: icon }]
-    })
+    head: () => ({
+        link: [{ hid: 'favicon', rel: 'icon', href: icon }],
+    }),
 }
 </script>
 
-<style lang="scss" >
+<style lang="scss">
 @import './../static/libs/theme';
 @media screen and (min-width: $desktop_breakpoints) {
-::-webkit-scrollbar{
-  width: 10px;
-  height: 10px;
-  background-color: #1b272f;
-  border-radius: 5px;
-  &-thumb{
-    background-color:#1b272f;
-    border-radius: 5px;
-  } 
-  &-track {
-    background: #566169;
-    }  
-  }
+    ::-webkit-scrollbar {
+        width: 10px;
+        height: 10px;
+        background-color: #1b272f;
+        border-radius: 5px;
+        &-thumb {
+            background-color: #1b272f;
+            border-radius: 5px;
+        }
+        &-track {
+            background: #566169;
+        }
+    }
 }
-html{
-  background-color: var(--light-primary);
-  height: 100%;
-} 
+html {
+    background-color: var(--light-primary);
+    height: 100%;
+}
 *,
 *::before,
-*::after{
-  box-sizing: border-box;
-  -webkit-box-sizing: border-box;
-  -o-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-} 
+*::after {
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -o-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
 
-body{
-  -webkit-font-smoothing: antialiased;
-  color: var(--text-color-primary);
-  font-family: 'Roboto', sans-serif;
-  min-height: 100vh;
-  display: flex;
-  overflow-x: hidden;
-  flex-wrap: nowrap;
-  flex-direction: column;
-  text-rendering: optimizeLegibility;
-}   
-main{
-  width: 100vw;
-  overflow-x: hidden;
+body {
+    -webkit-font-smoothing: antialiased;
+    color: var(--text-color-primary);
+    font-family: 'Roboto', sans-serif;
+    min-height: 100vh;
+    display: flex;
+    overflow-x: hidden;
+    flex-wrap: nowrap;
+    flex-direction: column;
+    text-rendering: optimizeLegibility;
 }
-a{
-  text-decoration: none;
-  outline: none;
-  transition: color .2s;
+main {
+    width: 100vw;
+    overflow-x: hidden;
 }
-li{
-  list-style: none;
+a {
+    text-decoration: none;
+    outline: none;
+    transition: color 0.2s;
 }
-img{
-  height: auto;
-  max-width: 100%;
+li {
+    list-style: none;
 }
-button{
-  outline: none;
-  border: none;
-  display: inline-block;
+img {
+    height: auto;
+    max-width: 100%;
 }
-input{
-  outline: none;
-  border: none;
+button {
+    outline: none;
+    border: none;
+    display: inline-block;
 }
-h1,h2,h3,h4,h5,h6 {
-  font-weight: normal;
+input {
+    outline: none;
+    border: none;
 }
-[role~="button"]{
-  cursor: pointer;
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    font-weight: normal;
 }
-</style>>
+[role~='button'] {
+    cursor: pointer;
+}</style
+>>
